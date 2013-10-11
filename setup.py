21d5089a836ba7f48b13c0a87e0b95da22e7eedb
Andrew Mussey <MusseyAM@vt.edu>
2013-10-11 13:31:41 -0400

    Added cqlsh and the associated cqlshlib.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='cqlsh',
+    version='1.0.0',
+    description=('A Python-based command-line client for running simple CQL commands on a Cassandra cluster'),
+    keywords='python cql cassandra cqlsh',
+    maintainer='Andrew Mussey',
+    maintainer_email='admin@ajama.org',
+    url='https://github.com/amussey/cqlsh',
+    install_requires=["cql"],
+    packages=['cqlshlib'],
+    scripts = [
+        'cqlsh',
+    ]
+)
