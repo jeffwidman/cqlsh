51ae6bbdd12a1125b61081c042ecc7fa36f3d217
Andrew Mussey <MusseyAM@vt.edu>
2013-10-14 11:17:02 -0400

    Added documentation to setup.py.

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
 
+'''A Python-based command-line client for running simple CQL commands on a Cassandra cluster
+
+cqlsh is a Python-based command-line tool, and the most direct way
+to run simple CQL commonds on a Cassandra cluster.  This is a simple
+re-bundling of the open source tool that comes bundled with Cassandra
+to allow for cqlsh to be installed and run inside of virtual
+environments.'''
+
 from distutils.core import setup
+
+doclines = __doc__.split("\n")
 
 setup(
     name='cqlsh',
-    version='1.0.0',
-    description=('A Python-based command-line client for running simple CQL commands on a Cassandra cluster'),
+    version='4.0.1',
+    description=doclines[0],
+    long_description='\n'.join(doclines[2:]),
     keywords='python cql cassandra cqlsh',
     maintainer='Andrew Mussey',
     maintainer_email='admin@ajama.org',
     url='https://github.com/amussey/cqlsh',
-    install_requires=[],
+    platforms=['any'],
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    install_requires=['cql', 'simplejson'],
     packages=['cqlshlib'],
     scripts = [
         'cqlsh',
