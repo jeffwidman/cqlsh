f100a72117b921bcb585a81c7e5f82942812c31c
Jeff Widman <jeff@jeffwidman.com>
2021-03-09 10:17:56 -0800

    Modernize PyPI config files
    Modernize the python package config files following the recommendations
    from https://packaging.python.org/tutorials/packaging-projects/ and
    taking inspiration from the excellent examples in the Flask project.

--- setup.py
+++ setup.py
@@ -1,31 +1,11 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-'''A Python-based command-line client for running simple CQL commands on a Cassandra cluster
-
-cqlsh is a Python-based command-line tool, and the most direct way
-to run simple CQL commonds on a Cassandra cluster.  This is a simple
-re-bundling of the open source tool that comes bundled with Cassandra
-to allow for cqlsh to be installed and run inside of virtual
-environments.'''
-
-from distutils.core import setup
-
-doclines = __doc__.split("\n")
-
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
-    name='cqlsh',
-    version='5.0.4',
-    description=doclines[0],
-    long_description='\n'.join(doclines[2:]),
-    keywords='python cql cassandra cqlsh',
-    maintainer='Spiro Sideris',
-    maintainer_email='spirosideris@gmail.com',
-    url='http://git-wip-us.apache.org/repos/asf/cassandra.git',
-    platforms=['any'],
-    license="http://www.apache.org/licenses/LICENSE-2.0",
-    install_requires=['cql', 'cassandra-driver'],
-    packages=['cqlshlib'],
-    scripts = [
-        'cqlsh',
-    ]
+    name="cqlsh",
+    install_requires=[
+        "cql",
+        "cassandra-driver",
+        "six",
+    ],
 )
